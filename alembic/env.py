cc31b6947c7bdbffe679cfcfbe6ad7e58445d04a
Alex <alexander7dk@gmail.com>
2023-04-16 21:02:18 -0700

    Set up alembic with DB_URL in .env file

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,5 @@
+import os
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -9,10 +11,14 @@ from alembic import context
 # access to the values within the .ini file in use.
 config = context.config
 
+section = config.config_ini_section
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+
+config.set_section_option(section, "DB_URL", os.environ.get("DB_URL"))
 
 # add your model's MetaData object here
 # for 'autogenerate' support
