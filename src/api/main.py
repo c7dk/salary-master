9084cdbfd30668f24a9613a22c5baf698f2ee9d6
Alex <alexander7dk@gmail.com>
2023-04-23 23:21:31 -0700

    attempted to connect jobs api to database but something was goofy

--- src/api/main.py
+++ src/api/main.py
@@ -1,15 +1,12 @@
 # main.py
-
+import json
 from fastapi import FastAPI
 import os
+from sqlalchemy import create_engine, text
 
 print(os.environ['DB_URL'])
 
 app = FastAPI()
-
-joblist=["Nurse", "Teacher", "Firefighter", "Police Officer", "Backend Worker"]
-
-print (len(joblist))
 
 @app.get("/")
 async def root():
@@ -17,12 +14,20 @@ async def root():
 
 @app.get("/jobs")
 async def jobs():
+    conn_string = os.environ.get("DB_URL")
+    engine = create_engine(conn_string)
+    with engine.connect() as conn:
+        sql=text("SELECT occ_title, a_mean, tot_emp FROM jobs WHERE a_mean > 100000 ORDER BY a_mean;")
+        result = conn.execute(sql)
+    joblist = [row.items() for row in result]
+    breakpoint()
+    return json.dumps(joblist)
     return joblist
 
-@app.get("/jobs/{job_id}")
-async def job(job_id):
-    job_id = int(job_id)
-    if job_id >= 0 and job_id < len(joblist):
-        return joblist[job_id]
-    else:
-        return "error"
+# @app.get("/jobs/{job_id}")
+# async def job(job_id):
+#     job_id = int(job_id)
+#     if job_id >= 0 and job_id < len(joblist):
+#         return joblist[job_id]
+#     else:
+#         return "error"
