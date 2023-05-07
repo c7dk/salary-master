48ceea908a7dad290c950c522d48952974e0136e
Alex <alexander7dk@gmail.com>
2023-05-06 22:10:26 -0700

    Removed a duplication inside api of main.py

--- src/api/main.py
+++ src/api/main.py
@@ -6,6 +6,9 @@ from sqlalchemy import create_engine, text
 
 print(os.environ['DB_URL'])
 
+conn_string = os.environ.get("DB_URL")
+engine = create_engine(conn_string)
+
 app = FastAPI()
 
 @app.get("/")
@@ -14,8 +17,6 @@ async def root():
 
 @app.get("/jobs")
 async def jobs():
-    conn_string = os.environ.get("DB_URL")
-    engine = create_engine(conn_string)
     with engine.connect() as conn:
         sql=text("SELECT occ_code, occ_title, a_mean::float, tot_emp FROM jobs WHERE a_mean > 100000 ORDER BY a_mean;")
         result = conn.execute(sql)
@@ -24,8 +25,6 @@ async def jobs():
 
 @app.get("/jobs/{job_id}")
 async def job(job_id):
-    conn_string = os.environ.get("DB_URL")
-    engine = create_engine(conn_string)
     with engine.connect() as conn:
         sql=text(f"SELECT occ_code, occ_title, a_mean::float, tot_emp FROM jobs WHERE occ_code = '{job_id}';")
         result = conn.execute(sql)
